7b5a4dbc99fb6f020dbf622f16bd8cff5e12b108
goal043 <lex@goal043.nl>
2015-07-09 14:15:03 +0200

    see if we actually comitted it

--- src/flambe/platform/html/HtmlAssetPackLoader.hx
+++ src/flambe/platform/html/HtmlAssetPackLoader.hx
@@ -206,7 +206,8 @@ class HtmlAssetPackLoader extends BasicAssetPackLoader
             }
             xhr = new XMLHttpRequest();
             xhr.open("GET", url, true);
-            xhr.responseType = responseType;
+            //xhr.responseType = responseType;
+			untyped xhr.responseType = responseType;
 
             var lastProgress = 0.0;
             xhr.onprogress = function (event :ProgressEvent) {
@@ -365,11 +366,11 @@ class HtmlAssetPackLoader extends BasicAssetPackLoader
             // request so it's all good.
             xhr.open("GET", ".", true);
 
-            if (xhr.responseType != "") {
+            if (untyped xhr.responseType != "") {
                 return false; // No responseType supported at all
             }
-            xhr.responseType = "blob";
-            if (xhr.responseType != "blob") {
+           untyped xhr.responseType = "blob";
+            if (untyped xhr.responseType != "blob") {
                 return false; // Blob responseType not supported
             }
 
